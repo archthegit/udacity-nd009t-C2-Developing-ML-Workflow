d0b7ab4ff6184bd6fc3edeab580c945d91a2c841
Archana Pradeep <archieprad@gmail.com>
2023-02-23 09:47:09 +0000

    finished project

---
+++ project/lambda.py
@@ -0,0 +1,92 @@
+#lambda 1
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event.get("s3_key")
+    bucket = event.get("s3_bucket")
+    
+    # Download the data from s3 to /tmp/image.png
+    s3.download_file(bucket, key, '/tmp/image.png')
+    
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
+
+
+# lambda 2
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+from sagemaker.predictor import Predictor
+
+# Fill this in with the name of your deployed model
+ENDPOINT = 'image-classification-2023-02-22-10-46-33-774'
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event["body"]["image_data"])
+
+    # Instantiate a Predictor
+    predictor = Predictor(ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(image)
+    
+    # We return the data back to the Step Function    
+    event["body"]["inferences"] = json.loads(inferences.decode('utf-8'))
+    return {
+        'statusCode': 200,
+        'body': event["body"]
+    }
+
+#lambda 3
+
+import json
+
+
+THRESHOLD = .93
+
+
+def lambda_handler(event, context):
+    
+    # Grab the inferences from the event
+    inferences = event["body"]["inferences"]
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any(map(lambda x: x > THRESHOLD, inferences))
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
